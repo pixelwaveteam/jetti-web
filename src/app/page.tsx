6e33e1a43c987d9fd16acae7e2b002bc97ddac76
JamDev0 <jamdeveloper0@gmail.com>
2024-01-16 11:54:41 -0300

    feat: index page redirecting according to the user state

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/options";
+
+export default async function IndexPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.accessToken) {
+    redirect(`/auth/signin`);
+  }
+
+  redirect('/dashboard')
+}
