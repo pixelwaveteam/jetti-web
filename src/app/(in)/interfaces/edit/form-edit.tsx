2c9c1139bd0595527beb479c3b991d0677080204
JamDev0 <jamdeveloper0@gmail.com>
2024-01-21 16:04:34 -0300

    fix: handling delete error in the wrong function at InterfaceFormEdit

--- src/app/(in)/interfaces/edit/form-edit.tsx
+++ src/app/(in)/interfaces/edit/form-edit.tsx
@@ -65,20 +65,7 @@ export function InterfaceFormEdit({
         description: 'Interface alterada com sucesso.',
         duration: 5000,
       });
-    } catch(err) {
-      if(err instanceof Error && err.message === "Interface has dependents.") {
-        toast({
-          variant: 'destructive',
-          title: 'Erro',
-          description: 'Essa interface tem registros associados à ela. Para exclui-la, exclua suas associações antes!',
-          duration: 7000,
-        });
-
-        setShow(false);
-
-        return
-      }
-
+    } catch {
       toast({
         variant: 'destructive',
         title: 'Erro',
@@ -100,7 +87,20 @@ export function InterfaceFormEdit({
         description: 'Interface excluida com sucesso.',
         duration: 5000,
       });
-    } catch {
+    } catch(err) {
+      if(err instanceof Error && err.message === "Interface has dependents.") {
+        toast({
+          variant: 'destructive',
+          title: 'Erro',
+          description: 'Essa interface tem registros associados à ela. Para exclui-la, exclua suas associações antes!',
+          duration: 7000,
+        });
+
+        setShow(false);
+
+        return
+      }
+      
       toast({
         variant: 'destructive',
         title: 'Erro',
