64ae8f99f594621f08f20dc013c90b60249c2070
JamDev0 <jamdeveloper0@gmail.com>
2024-01-21 15:40:06 -0300

    feat: handling delete error when interface has dependents at InterfaceFormEdit

--- src/app/(in)/interfaces/edit/form-edit.tsx
+++ src/app/(in)/interfaces/edit/form-edit.tsx
@@ -65,7 +65,20 @@ export function InterfaceFormEdit({
         description: 'Interface alterada com sucesso.',
         duration: 5000,
       });
-    } catch {
+    } catch(err) {
+      if(err instanceof Error && err.message === "Interface has dependents.") {
+        toast({
+          variant: 'destructive',
+          title: 'Erro',
+          description: 'Essa interface tem registros associados à ela. Para exclui-la, exclua suas associações antes!',
+          duration: 7000,
+        });
+
+        setShow(false);
+
+        return
+      }
+
       toast({
         variant: 'destructive',
         title: 'Erro',
