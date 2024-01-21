8e1899bd999fb3a632307409ffae60b365347569
JamDev0 <jamdeveloper0@gmail.com>
2024-01-21 16:03:12 -0300

    fix: handling delete error in the wrong function at EstablishmentFormEdit

--- src/app/(in)/establishments/[id]/edit/form-edit.tsx
+++ src/app/(in)/establishments/[id]/edit/form-edit.tsx
@@ -81,24 +81,11 @@ export function EstablishmentFormEdit({
         description: 'Local alterado com sucesso.',
         duration: 5000,
       });
-    } catch(err) {
-      if(err instanceof Error && err.message === "Establishment has dependents.") {
-        toast({
-          variant: 'destructive',
-          title: 'Erro',
-          description: 'Esse local tem registros associados à ele. Para exclui-lo, exclua suas associações antes!',
-          duration: 7000,
-        });
-
-        setShow(false);
-
-        return
-      }
-
+    } catch {
       toast({
         variant: 'destructive',
         title: 'Erro',
-        description: 'Favor tente novamente mais tarde.',
+        description: 'Favor tente novamenteDSAD mais tarde.',
         duration: 5000,
       });
     }
@@ -118,7 +105,20 @@ export function EstablishmentFormEdit({
       });
 
       router.push(`/establishments`);
-    } catch {
+    } catch(err) {
+      if(err instanceof Error && err.message === "Establishment has dependents.") {
+        toast({
+          variant: 'destructive',
+          title: 'Erro',
+          description: 'Esse local tem registros associados à ele. Para exclui-lo, exclua suas associações antes!',
+          duration: 7000,
+        });
+
+        setShow(false);
+
+        return
+      }
+
       toast({
         variant: 'destructive',
         title: 'Erro',
