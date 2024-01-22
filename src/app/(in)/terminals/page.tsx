deafb9dbf3943a458b60d387d8bb24886a552eb6
JamDev0 <jamdeveloper0@gmail.com>
2024-01-21 22:36:51 -0300

    feat: added terminals establishment to terminals object at Terminals page

--- src/app/(in)/terminals/page.tsx
+++ src/app/(in)/terminals/page.tsx
@@ -14,10 +14,22 @@ export const metadata: Metadata = {
 };
 
 export default async function Terminals() {
-  const terminals = await fetchTerminals();
+  const rawTerminals = await fetchTerminals();
   const organizations = await fetchOrganizations();
   const establishments = await fetchEstablishments();
   const interfaces = await fetchInterfaces();
+
+  const terminals = [];
+
+  for(const terminal of rawTerminals) {
+    const establishment = establishments.find(establishment => establishment.id === terminal.establishmentId)
+
+    if(!establishment) {
+      throw new Error(`No establishment found with id ${terminal.establishmentId}`)
+    }
+
+    terminals.push({...terminal, establishment: establishment.name})
+  }
 
   return (
     <PageContainer title='Terminais'>
