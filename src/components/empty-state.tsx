695df89237c9bdd07241522250581a844c41cba3
JamDev0 <jamdeveloper0@gmail.com>
2024-01-22 19:37:07 -0300

    feat: Extended EmptyState's props by ButtonHTMLAttributes

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -1,16 +1,17 @@
 import { cx } from 'class-variance-authority';
 import { Plus } from 'lucide-react';
+import { ButtonHTMLAttributes } from 'react';
 
-interface EmptyStateProps {
+interface EmptyStateProps extends ButtonHTMLAttributes<HTMLButtonElement> { 
   label: string;
-  className?: string;
 }
 
-export function EmptyState({ label, className }: EmptyStateProps) {
+export function EmptyState({ label, className, ...props }: EmptyStateProps) {
   return (
     <button
       type='button'
       className={cx(className, 'relative flex gap-4 items-center justify-center w-full rounded-md border border-dashed border-gray-500 p-12 text-center hover:border-gray-400 hover:bg-gray-900 transition-all')}
+      {...props}
     >
       <Plus size={24} />
       <span className='font-semibold'>{label}</span>
