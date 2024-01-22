be8292020f5142d3be5ed4fa21d6f662304d2c54
JamDev0 <jamdeveloper0@gmail.com>
2024-01-21 22:04:23 -0300

    feat: columns state, city, terminalsTotal added to establishmentColumns

--- src/app/(in)/establishments/columns.tsx
+++ src/app/(in)/establishments/columns.tsx
@@ -11,6 +11,9 @@ const EstablishmentSchema = z.object({
   id: z.string(),
   organizationId: z.string(),
   name: z.string(),
+  state: z.string().optional(),
+  city: z.string().optional(),
+  terminalsTotal: z.number().optional().default(0),
 });
 
 export type EstablishmentData = z.infer<typeof EstablishmentSchema>;
@@ -18,6 +21,7 @@ export type Establishment = {
   id: string;
   organizationId: string;
   name: string;
+  state?: string;
 };
 
 export const establishmentColumns: ColumnDef<EstablishmentData>[] = [
@@ -42,6 +46,81 @@ export const establishmentColumns: ColumnDef<EstablishmentData>[] = [
           <span className='truncate'>{establishment.name}</span>
         </div>
       );
+    },
+  },
+  {
+    accessorKey: 'state',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Estado
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const establishment = row.original;
+
+      if(establishment.state) {
+        return (
+          <div className='flex gap-2 items-center'>
+            <span className='truncate'>{establishment.state}</span>
+          </div>
+        )
+      }
+    },
+  },
+  {
+    accessorKey: 'city',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Cidade
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const establishment = row.original;
+
+      if(establishment.city) {
+        return (
+          <div className='flex gap-2 items-center'>
+            <span className='truncate'>{establishment.city}</span>
+          </div>
+        )
+      }
+    },
+  },
+  {
+    accessorKey: 'terminalsTotal',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Terminais
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const establishment = row.original;
+
+      if(establishment.terminalsTotal) {
+        return (
+          <div className='flex gap-2 items-center'>
+            <span className='truncate'>{establishment.terminalsTotal}</span>
+          </div>
+        )
+      }
     },
   },
   {
