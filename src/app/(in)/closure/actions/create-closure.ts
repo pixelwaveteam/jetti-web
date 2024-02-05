3c2d29eb871c49b8e23fb0481e047dd0d646b5e2
JamDev0 <jamdeveloper0@gmail.com>
2024-02-05 19:03:33 -0300

    feat: add return type to createClosure action

--- src/app/(in)/closure/actions/create-closure.ts
+++ src/app/(in)/closure/actions/create-closure.ts
@@ -3,13 +3,14 @@
 import { revalidateTag } from 'next/cache';
 
 import { api } from '@/lib/api';
+import { Closure } from './fetch-closure';
 
 interface CreateClosure {
   cashFlows: string
 }
 
 export async function createClosure(data: CreateClosure) {
-  const response = await api('/closures', {
+  const response = await api<Closure>('/closures', {
     method: 'POST',
     body: JSON.stringify(data),
   });
