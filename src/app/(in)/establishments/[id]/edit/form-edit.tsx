1ffb1b7db4c2a45f144746816424891614b26efb
JamDev0 <jamdeveloper0@gmail.com>
2024-03-04 17:26:13 -0300

    feat: change isWarehouse inputs name in establishemnts forms

--- src/app/(in)/establishments/[id]/edit/form-edit.tsx
+++ src/app/(in)/establishments/[id]/edit/form-edit.tsx
@@ -210,7 +210,7 @@ export function EstablishmentFormEdit({
             render={(({ field }) => (
               <FormItem className='flex items-center gap-x-6'>
                 <div className='space-y-0.5'>
-                  <FormLabel className='text-base'>Esta no Galpão</FormLabel>
+                  <FormLabel className='text-base'>Galpão</FormLabel>
                 </div>
                 <FormControl>
                   <Switch onCheckedChange={field.onChange} checked={field.value} />
