dace7c8b586fd1a00207c4be4378f59f0993cbdc
JamDev0 <jamdeveloper0@gmail.com>
2024-09-03 14:59:42 -0300

    fix: edit cashflow default values

--- src/app/(in)/cash-flows/[id]/edit/form-edit.tsx
+++ src/app/(in)/cash-flows/[id]/edit/form-edit.tsx
@@ -81,8 +81,8 @@ export function CashFlowFormEdit({ cashFlow }: CashFlowFormEditProps) {
     defaultValues: {
       terminalId: cashFlow.terminalId,
       establishmentId: cashFlow.establishmentId,
-      input: cashFlow.input/100,
-      output: cashFlow.output/100,
+      input: cashFlow.cashIn/100,
+      output: cashFlow.cashOut/100,
       date: new Date(cashFlow.date),
     },
   });
