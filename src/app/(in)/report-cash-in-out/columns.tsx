25e81f2a15f6948d490453e995f4b7f931f74416
JAMDev <61752887+JamDev0@users.noreply.github.com>
2024-09-05 08:55:53 -0300

    Update columns.tsx

--- src/app/(in)/report-cash-in-out/columns.tsx
+++ src/app/(in)/report-cash-in-out/columns.tsx
@@ -14,30 +14,6 @@ export type ReportCashInOutDataTableData = CashFlow & {
 
 export const cashFlowColumns: ColumnDef<ReportCashInOutDataTableData>[] = [
   {
-    accessorKey: 'code',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant='ghost'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          id
-          <ArrowUpDown className='ml-2 h-4 w-4' />
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      const cashFlow = row.original;
-
-      return (
-        <div className='flex flex-col gap-2 items-start'>
-          <span>{cashFlow.code}</span>
-        </div>
-      );
-    },
-  },
-  
-  {
     accessorKey: 'organization',
     filterFn: (row, id, value) => {
       return value.length > 0 ? value.includes(row.original.organization) : true;
